b0ff823ac8c1c43ba63646f91cb2abc873a1d8c5
Tomasz Morawski <tmorawski2@gmail.com>
2019-04-17 23:02:12 +0200

    initial

--- src/main/java/org/morawski/poi/BaseFileHandler.java
+++ src/main/java/org/morawski/poi/BaseFileHandler.java
@@ -3,6 +3,7 @@ package org.morawski.poi;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Color;
 import org.apache.poi.ss.usermodel.ExtendedColor;
 import org.apache.poi.ss.usermodel.FillPatternType;
@@ -23,10 +24,13 @@ public class BaseFileHandler {
 		
 		Row r;
 		
+		
 		Cell c;
+		
+		
 		CellStyle styleHeader=workbook.createCellStyle();
 		CellStyle stylenormalRow=workbook.createCellStyle();
-		
+		//c.setCellType(CellType.STRING);
 		
 		//konfiguracja kolorów
 		XSSFColor xssFColorBg = new XSSFColor(new DefaultIndexedColorMap());
@@ -37,18 +41,18 @@ public class BaseFileHandler {
 		
 		
 		//ustawienie kolorów nagłówka
-		styleHeader.setFillBackgroundColor(xssFColorBg.getIndexed());
-		styleHeader.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		styleHeader.setLocked(true);
+		styleHeader.setFillForegroundColor(xssFColorBg.getIndexed()); //tło
+		styleHeader.setFillPattern(FillPatternType.SOLID_FOREGROUND); 
+		//styleHeader.setLocked(true);
 		
 		Font headerFont = workbook.createFont();
-		headerFont.setColor(xssFColorFg.getIndexed());
+		headerFont.setColor(xssFColorFg.getIndexed()); //ustawienie koloru czionki
 		headerFont.setBold(true);
 		headerFont.setFontName("Calibri");
 		styleHeader.setFont(headerFont);
-		//styleHeader.setFillForegroundColor(xssFColorFg.getIndexed());
+
 		
-		//styleHeader.setFillBackgroundColor(arg0);
+		sheet.createFreezePane(0, 1);
 	}
 
 
